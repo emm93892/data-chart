5a69672ab1a49fdf7e049d1ccfd73ca5c5ca78f3
加藤康太 <emm93892@emm93892-macbook.local>
2020-02-06 12:19:19 +0900

    プロジェクトの作成

---
+++ src/test/java/jp/co/chitose/DataChartApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.chitose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataChartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
